6b3f1ffd511e9bcf32708dc71b1ba0fff676c8b3
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2020-10-16 09:04:12 +0200

    feat: add amin privilege

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ListUser from "./pages/ListUser";
 import Layout from "./components/Layout";
 import ListTransactions from "./pages/ListOfTransactions";
+import { onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { Auth, API } from "aws-amplify";
 
 function App() {
+  const [authState, setAuthState] = useState();
+  const [user, setUser] = useState();
+  const [groups, setGroups] = useState([]);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    async function fetchData(user) {
+      const apiName = "AdminQueries";
+      const path = "/listGroupsForUser";
+      const myInit = {
+        queryStringParameters: {
+          username: user.username,
+        },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${(await Auth.currentSession())
+            .getAccessToken()
+            .getJwtToken()}`,
+        },
+      };
+      const { NextToken, ...rest } = await API.get(apiName, path, myInit);
+      setGroups(rest.Groups);
+      const findAdmin = rest.Groups.find(
+        (group) => group.GroupName === "admin"
+      );
+      setIsAdmin(findAdmin ? true : false);
+    }
+
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+      fetchData(authData);
+    });
+  }, [setAuthState]);
+
   return (
     <div className="App">
       <Router>
         <Switch>
           <Layout>
-            <Route path="/listUser" exact>
-              <ListUser />
-            </Route>
-            <Route path="/listTransactions" exact>
-              <ListTransactions />
-            </Route>
+            {isAdmin ? (
+              <>
+                <Route path="/listUser" exact>
+                  <ListUser />
+                </Route>
+                <Route path="/listTransactions" exact>
+                  <ListTransactions />
+                </Route>
+              </>
+            ) : (
+              "You need admin privilege"
+            )}
           </Layout>
         </Switch>
       </Router>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,61 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { NavLink } from "react-router-dom";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import { AmplifySignOut } from "@aws-amplify/ui-react";
-import { onAuthUIStateChange } from "@aws-amplify/ui-components";
-import { Auth, API } from "aws-amplify";
 
 export default function Menu() {
-  const [authState, setAuthState] = useState();
-  const [user, setUser] = useState();
-  const [groups, setGroups] = useState([]);
-  const [isAdmin, setIsAdmin] = useState(false);
-
-  useEffect(() => {
-    async function fetchData(user) {
-      const apiName = "AdminQueries";
-      const path = "/listGroupsForUser";
-      const myInit = {
-        queryStringParameters: {
-          username: user.username,
-        },
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${(await Auth.currentSession())
-            .getAccessToken()
-            .getJwtToken()}`,
-        },
-      };
-      const { NextToken, ...rest } = await API.get(apiName, path, myInit);
-      setGroups(rest.Groups);
-      const findAdmin = rest.Groups.find(
-        (group) => group.GroupName === "admin"
-      );
-      setIsAdmin(findAdmin ? true : false);
-    }
-
-    return onAuthUIStateChange((nextAuthState, authData) => {
-      setAuthState(nextAuthState);
-      setUser(authData);
-      fetchData(authData);
-    });
-  }, [setAuthState]);
-
   return (
     <Grid item xs={12} container direction="column" justify="space-between">
       <List style={{ marginTop: "40px" }}>
         <ListItem>
           <AmplifySignOut />
         </ListItem>
-        {isAdmin ? (
-          <NavLink to="/listUser" exact>
-            <ListItem button>List of user</ListItem>
-          </NavLink>
-        ) : (
-          ""
-        )}
+        <NavLink to="/listUser" exact>
+          <ListItem button>List of user</ListItem>
+        </NavLink>
         <NavLink to="/listTransactions" exact>
           <ListItem button>List of transactions</ListItem>
         </NavLink>
